411425bdc6a42f397b1be8fcd101653901d20be8
michaellgans <michaellgans@Victoria>
2024-09-08 16:11:22 -0500

    MLG - 4:11 PM - properly imports main.tsx

--- src/MusicPlayer.tsx
+++ src/MusicPlayer.tsx
@@ -1,11 +1,18 @@
 // Music Player Component
 
+import React from "react";
+
 // Asset Imports
 
 // Script Imports
 
+// Types
+type MusicPlayerProps = {
+  children: React.ReactNode;
+};
+
 // Returns a MusicPlayer Component
-export default function MusicPlayer({ children }) {
+export default function MusicPlayer({ children }: MusicPlayerProps) {
   // Define Hooks
   return (
     <div className="music-player dark:bg-dark-player-background bg-player-background flex flex-col md:flex-row max-w-4xl justify-center rounded-lg shadow-lg">
