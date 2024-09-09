e7624a1ba7e87a8130571a2411f2d1248c6dd6c0
michaellgans <michaellgans@Victoria>
2024-09-09 13:33:18 -0500

    MLG - 1:33 PM - pulls cover art from API

--- src/components/CoverArt.tsx
+++ src/components/CoverArt.tsx
@@ -1,17 +1,35 @@
 // CoverArt Component
 
 // Asset Imports
+import { useState, useEffect } from "react";
 import artSkeleton from "../assets/placeholder.svg";
 
 // Script Imports
 
+// Type
+type Art = {
+  cover: string;
+};
+
 // Returns a CoverArt Component
 export function CoverArt() {
   // Define Hook
+  const [art, setArt] = useState<Art>({ cover: artSkeleton });
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/atlas-jswank/atlas-music-player-api/main/playlist",
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setArt({ cover: data[0].cover });
+      });
+  }, []);
+
   return (
     <div className="cover-art mb-6">
       <img
-        src={artSkeleton}
+        src={art.cover || artSkeleton}
         alt="Cover Art for Current Song"
         className="cover-art-image rounded-md"
       />
