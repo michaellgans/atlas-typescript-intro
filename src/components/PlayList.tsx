f512a86402686c415ddc73098549b427fd014bb7
michaellgans <michaellgans@Victoria>
2024-09-09 13:07:31 -0500

    MLG - 1:07 PM - Creates playlist items based on API

--- src/components/PlayList.tsx
+++ src/components/PlayList.tsx
@@ -2,59 +2,45 @@
 
 // Asset Imports
 import { PlayListItem } from "./PlayListItem";
+import { useState, useEffect } from "react";
 
 // Script Imports
+
+// Type
+type Song = {
+  title: string;
+  artist: string;
+  duration: string;
+};
 
 // Returns a PlayList Component
 export function PlayList() {
   // Define Hook
+  const [songs, setSongs] = useState<Song[]>([]);
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/atlas-jswank/atlas-music-player-api/main/playlist",
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setSongs(data);
+      });
+  }, []);
+
   return (
     <div className="play-list w-full p-6 md:w-1/2 md:border-l md:border-muted-text dark:md:border-dark-muted-text">
-      <h3 className="play-list-title mb-4 text-xl font-semibold">
-        What's On Your Vest?
-      </h3>
+      <h3 className="play-list-title mb-4 text-xl font-semibold">Playlist</h3>
       <div className="all-songs flex flex-col pr-4">
-        <PlayListItem
-          songTitle="Rainbow in the Dark"
-          artist="Dio"
-          playTime="4:17"
-        />
-        <PlayListItem
-          songTitle="Wild Child"
-          artist="W.A.S.P."
-          playTime="5:12"
-        />
-        <PlayListItem
-          songTitle="Breaking the Law"
-          artist="Judas Priest"
-          playTime="2:34"
-        />
-        <PlayListItem songTitle="Overkill" artist="Motorhead" playTime="5:11" />
-        <PlayListItem
-          songTitle="Born To Raise Hell"
-          artist="Motorhead"
-          playTime="5:00"
-        />
-        <PlayListItem
-          songTitle="You've Got Another Thing Coming"
-          artist="Judas Priest"
-          playTime="5:10"
-        />
-        <PlayListItem
-          songTitle="Heading Out to the Highway"
-          artist="Judas Priest"
-          playTime="3:46"
-        />
-        <PlayListItem
-          songTitle="L.O.V.E. Machine"
-          artist="W.A.S.P."
-          playTime="3:52"
-        />
-        <PlayListItem
-          songTitle="On Your Knees"
-          artist="W.A.S.P."
-          playTime="3:49"
-        />
+        {songs.map((song, index) => (
+          <PlayListItem
+            key={index}
+            songTitle={song.title}
+            artist={song.artist}
+            playTime={song.duration}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/PlayListItem.tsx
+++ src/components/PlayListItem.tsx
@@ -18,6 +18,7 @@ export function PlayListItem({
   playTime,
 }: PlayListItemProps) {
   // Define Hook
+
   return (
     <div className="play-list-item mb-1 flex h-10 cursor-pointer items-center justify-between rounded-md text-sm font-medium first:bg-hover hover:bg-hover active:bg-active dark:first:bg-dark-hover dark:hover:bg-dark-hover dark:active:bg-dark-active">
       <div className="song-info">
