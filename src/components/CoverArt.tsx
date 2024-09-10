c9efcce8e6941b9af8e2d4fa53f26626eb6a9aae
michaellgans <michaellgans@Victoria>
2024-09-10 12:08:38 -0500

    MLG - 12:08 PM - fixes cover art skeleton

--- src/components/CoverArt.tsx
+++ src/components/CoverArt.tsx
@@ -4,6 +4,7 @@
 import { useEffect, useState } from "react";
 import { useContext } from "react";
 import { AppContext } from "./ContextProvider";
+import imageSkeleton from "../assets/placeholder.svg";
 
 // Script Imports
 
@@ -41,17 +42,11 @@ export function CoverArt() {
         />
       ) : (
         // Loading Skeleton
-        <div className="cover-skeleton">
-          <svg
-            className="text-gray-200 dark:text-gray-600 h-full w-full animate-pulse"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 18"
-          >
-            <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
-          </svg>
-        </div>
+        <img
+          src={imageSkeleton}
+          alt="Placeholder for Loading"
+          className="image-skeleton animate-pulse rounded-md"
+        />
       )}
     </div>
   );
