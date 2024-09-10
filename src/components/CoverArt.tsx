161279fc4c2b1684d4e9d30fcf2abf4daf756c43
michaellgans <michaellgans@Victoria>
2024-09-10 11:38:38 -0500

    MLG - 11:37 AM - styles skeleton

--- src/components/CoverArt.tsx
+++ src/components/CoverArt.tsx
@@ -2,6 +2,8 @@
 
 // Asset Imports
 import { useState, useEffect } from "react";
+import { useContext } from "react";
+import { AppContext } from "./components/ContextProvider";
 
 // Script Imports
 
@@ -15,16 +17,6 @@ export function CoverArt() {
   // Define Hook
   const [art, setArt] = useState<Art | null>(null);
 
-  useEffect(() => {
-    fetch(
-      "https://raw.githubusercontent.com/atlas-jswank/atlas-music-player-api/main/playlist",
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setArt({ cover: data[0].cover });
-      });
-  }, []);
-
   return (
     <div className="cover-art mb-6">
       {art ? (
@@ -35,22 +27,16 @@ export function CoverArt() {
         />
       ) : (
         // Loading Skeleton
-        <div
-          role="status"
-          className="animate-pulse space-y-8 md:flex md:items-center md:space-x-8 md:space-y-0 rtl:space-x-reverse"
-        >
-          <div className="bg-gray-300 dark:bg-gray-700 flex h-48 w-full items-center justify-center rounded sm:w-96">
-            <svg
-              className="text-gray-200 dark:text-gray-600 h-10 w-10"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 18"
-            >
-              <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
-            </svg>
-          </div>
-          <span className="sr-only">Loading...</span>
+        <div className="cover-skeleton">
+          <svg
+            className="text-gray-200 dark:text-gray-600 h-full w-full animate-pulse"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 18"
+          >
+            <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+          </svg>
         </div>
       )}
     </div>
